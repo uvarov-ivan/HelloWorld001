f675a2c1512ea9342053e79769bf9edc336c3fc4
Ivan Uvarov <uvarov_ivan@rambler.ru>
2023-02-10 12:35:56 +0300

    Added greetings users

---
+++ example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ох ты ж нах, это ж Машка!");   
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
